64e261f4b774cee052d0238e2f9565dbd64efc96
previtus <ruzickav@phys.ethz.ch>
2019-06-06 21:39:19 +0200

    Experiments with Entropy and BALD as an alternative to the Variance metric (however they seem to be slow)

--- try_with_MC_BatchNorm_Idea2asloop.py
+++ try_with_MC_BatchNorm_Idea2asloop.py
@@ -128,15 +128,96 @@ def main(args):
     predictions_N = len(predictions_for_sample[0])
 
     print("predictions_N:", predictions_N)
+    import scipy
 
     for prediction_i in range(predictions_N):
         predictions = predictions_for_sample_By_Images[prediction_i]  # 5 x 256x256
 
-        entropy_image = None
-        sum_ent = 0
-        # incorrect calculation of entropy btw
-        ##entropy_image = np.apply_along_axis(arr=predictions, axis=0, func1d=entropy) # <<< The slow one ...
-        ##sum_ent = np.sum(entropy_image.flatten())
+        a_problematic_zone = np.finfo(float).eps # move 0-1 to 0.1 to 0.9
+        helper_offset = np.ones_like(predictions)
+        predictions = predictions * (1.0 - 2*a_problematic_zone) + helper_offset * (a_problematic_zone)
+
+        def entropy_across_predictions(pixel_predictions):
+            #print("pixel_predictions.shape", pixel_predictions.shape)
+            T = len(pixel_predictions)
+            p_sum = np.sum(pixel_predictions, axis=0)
+            #assert len(pixel_predictions.shape) == 1
+
+            pk0 = ( p_sum ) / T
+            pk1 = 1 - ( p_sum ) / T
+
+            entropy0 = - pk0 * np.log(pk0)
+            entropy1 = - pk1 * np.log(pk1) # i think that this one can be ignored in two class case ... in theory ...
+
+            """
+            print("pk0", pk0)
+            print("pk1", pk1)
+            print("entropy0", entropy0)
+            print("entropy1", entropy1)
+            """
+            return entropy0 + entropy1
+
+        def ent_img_sumDiv(pixel_predictions):
+            return np.sum(pixel_predictions, axis=0) / len(pixel_predictions)
+        def ent_img_log(pk):
+            return - pk * np.log(pk)
+
+        startTMP = timer()
+        # trying to write it faster !
+
+        ent_img_pk0 = np.apply_along_axis(arr=predictions, axis=0, func1d=ent_img_sumDiv)
+        ent_img_pk1 = np.ones_like(ent_img_pk0) - ent_img_pk0
+        ent_img_ent0 = np.apply_along_axis(arr=ent_img_pk0, axis=0, func1d=ent_img_log)
+        ent_img_ent1 = np.apply_along_axis(arr=ent_img_pk1, axis=0, func1d=ent_img_log)
+        entropy_image = ent_img_ent0 + ent_img_ent1
+        sum_ent = np.sum(entropy_image.flatten())
+
+        endTMP = timer()
+        timeTMP = (endTMP - startTMP)
+        print("Entropy faster " + str(timeTMP) + "s (" + str(timeTMP / 60.0) + "min)")
+
+        """
+        Entropy faster 0.28297295499942265s (0.004716215916657044min)
+        Entropy before 0.481015188008314s (0.008016919800138567min)
+
+        startTMP = timer()
+
+        entropy_image = np.apply_along_axis(arr=predictions, axis=0, func1d=entropy_across_predictions)
+        sum_ent = np.sum(entropy_image.flatten())
+
+        endTMP = timer()
+        timeTMP = (endTMP - startTMP)
+        print("Entropy before " + str(timeTMP) + "s (" + str(timeTMP / 60.0) + "min)")
+        """
+
+        def BALD_diff(pixel_predictions):
+            # Bayesian Active Learning by Disagreement = BALD = https://arxiv.org/abs/1112.5745
+            #T = len(pixel_predictions)
+            #assert len(pixel_predictions.shape) == 1
+
+            accum = 0
+            for val in pixel_predictions:
+                #if val == 0.0:
+                #    val += np.finfo(float).eps
+                #elif val == 1.0:
+                #    val -= np.finfo(float).eps
+
+                accum0 = - val * np.log(val)
+                accum1 = - (1-val) * np.log(1-val)
+                accum += accum0 + accum1
+
+            return accum
+
+        startTMP = timer()
+
+        bald_diff_image = np.apply_along_axis(arr=predictions, axis=0, func1d=BALD_diff)
+
+        endTMP = timer()
+        timeTMP = (endTMP - startTMP)
+        print("Bald orig" + str(timeTMP) + "s (" + str(timeTMP / 60.0) + "min)")
+
+        bald_image = -1 * ( entropy_image - bald_diff_image )
+        sum_bald = np.sum(bald_image.flatten())
 
         variance_image = np.var(predictions, axis=0)
         sum_var = np.sum(variance_image.flatten())
@@ -146,17 +227,25 @@ def main(args):
             fig = plt.figure(figsize=(10, 8))
             for i in range(T):
                 img = predictions[i]
-                ax = fig.add_subplot(1, T + 1, i + 1)
-                plt.imshow(img, cmap='gray')
+                ax = fig.add_subplot(1, T + 3, i + 1)
+                plt.imshow(img, cmap='gray', vmin = 0.0, vmax = 1.0)
                 ax.title.set_text('Model ' + str(i))
 
-            #ax = fig.add_subplot(1, T + 2, T + 1)
-            #plt.imshow(variance_image, cmap='gray')
-            #ax.title.set_text('XYZ Todo Entropy Viz (' + str(sum_ent) + ')')
+            ax = fig.add_subplot(1, T + 3, T + 1)
+            plt.imshow(entropy_image, cmap='gray', vmin = 0.0, vmax = 1.0)
+            ax.title.set_text('Entropy (' + str(np.round(sum_ent,3)) + ')')
 
-            ax = fig.add_subplot(1, T + 1, T + 1)
-            plt.imshow(variance_image, cmap='gray')
-            ax.title.set_text('Variance Viz (' + str(sum_var) + ')')
+            #ax = fig.add_subplot(1, T + 3, T + 2)
+            #plt.imshow(entropy_image_f, cmap='gray', vmin = 0.0, vmax = 1.0)
+            #ax.title.set_text('Entropy_f (' + str(np.round(sum_ent_f,3)) + ')')
+
+            ax = fig.add_subplot(1, T + 3, T + 2)
+            plt.imshow(bald_image, cmap='gray') #, vmin = 0.0, vmax = 1.0)
+            ax.title.set_text('BALD (' + str(np.round(sum_bald,3)) + ')')
+
+            ax = fig.add_subplot(1, T + 3, T + 3)
+            plt.imshow(variance_image, cmap='gray', vmin = 0.0, vmax = 1.0)
+            ax.title.set_text('Variance (' + str(np.round(sum_var,3)) + ')')
 
             plt.show()
 
